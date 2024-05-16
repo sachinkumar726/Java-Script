6e39ee4e578c9dd0b4371aaa1533c6b9430d320b
Sachin Kumar <139908913+sachinkumar726@users.noreply.github.com>
2024-05-16 13:14:55 +0530

    Data conversion

---
+++ DatatypeConversion.js
@@ -0,0 +1,31 @@
+// let score =null
+// let score1 = "33aa"
+// console.log(typeof score);
+// console.log(typeof score1)
+
+
+// let valueInNumber = Number(score1)
+// console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+
+// console.log(score);
+
+// "33" => 33 In number
+// "33abc" => NaN
+// true => 1; false => 0
+
+
+let isLoggedIn = ""
+let booleanIsLoggIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggIn)
+
+// 1 => true ; 0 => false
+// "" => false
+// "hitesh" => true
+
+
+let someNumber = 33
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
