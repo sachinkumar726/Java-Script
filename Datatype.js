7acb30c3f60cf2f65eca923efc1f51854ee35a42
Sachin Kumar <139908913+sachinkumar726@users.noreply.github.com>
2024-05-16 12:58:13 +0530

    data type

---
+++ Datatype.js
@@ -0,0 +1,24 @@
+"use strict"// treat all js code as newer version
+
+// alert(3+3) // we are using nodejs , not browser
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// boolean => true/false
+// null standalone value let state = null
+// undefine for ex let name;
+// symbol => for uniqness
+
+// object 
+
+console.log(typeof "Sachin");
+
+console.log(typeof null) // null is a object
+
+
+
+
---
+++ first.js
@@ -0,0 +1,14 @@
+ const accountId = 12345
+ let accoutEmail = 'sachin123@gmail.com'
+ var accoutPassword =  '1234'
+ accountCity = 'jaipur'
+
+ accoutEmail ="kumar"
+ //console.log(accountId);
+ console.log(accoutPassword);
+ console.table([accountId,accoutPassword,accoutEmail,accountCity])
+ 
+ /*
+ prefer not to use var
+ because of issue in block scope and functional scope
+  */
