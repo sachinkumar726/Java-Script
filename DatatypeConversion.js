0e7af390fefcbecc6d2aafd9712262a6bde4c4d8
Sachin Kumar <139908913+sachinkumar726@users.noreply.github.com>
2024-05-16 13:36:40 +0530

    Datatype

--- DatatypeConversion.js
+++ DatatypeConversion.js
@@ -15,17 +15,51 @@
 // true => 1; false => 0
 
 
-let isLoggedIn = ""
-let booleanIsLoggIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggIn)
+// let isLoggedIn = ""
+// let booleanIsLoggIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggIn)
 
 // 1 => true ; 0 => false
 // "" => false
 // "hitesh" => true
 
 
-let someNumber = 33
-let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let someNumber = 33
+// let stringNumber = String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *************** OPeration ************************88
+let value = 3
+let negValue = -value
+
+// console.log(negValue)
+
+// // console.log(2+2);
+// // console.log(2-2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = "hitesh"
+
+let str3 = str1 + str2 
+console.log(str3);
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2)
+// console.log(1 + 2 + "2")
+
+//console.log((3+4) * 5 % 3);
+
+// console.log(+true)
+// console.log(+"")
+
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+
+
 
