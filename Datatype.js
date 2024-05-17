10d834d72a0473b078287688a6e3bee00e02aa4c
Sachin Kumar <139908913+sachinkumar726@users.noreply.github.com>
2024-05-17 13:16:10 +0530

    Data type

--- Datatype.js
+++ Datatype.js
@@ -2,9 +2,9 @@
 
 // alert(3+3) // we are using nodejs , not browser
 
-let name = "hitesh"
-let age = 18
-let isLoggedIn = false
+// let name = "hitesh"
+// let age = 18
+// let isLoggedIn = false
 
 // number => 2 to power 53
 // bigint
@@ -15,10 +15,44 @@ let isLoggedIn = false
 
 // object 
 
-console.log(typeof "Sachin");
+// console.log(typeof "Sachin");
 
-console.log(typeof null) // null is a object
+// console.log(typeof null) // null is a object
 
 
+//***************************************************************************** */
+// primitive 
+// 7 types : String , Number , Boolean, null, undefine, Symbol, BigInt
+
+const score =100;
+
+const scoreValue = 100.3;
+
+const isLoggedIn = false;
+const outSideTemp = null
+let userEmail;
+
+const id = Symbol('123');
+const anotherId = Symbol('123');
+
+//ceck
+console.log(id === anotherId)
+
+const bigNumber = 865432135412n;
+
+
+// Reference (non primitive)
+// Array, Objects, Functions
+
+const heros = ["sachin", "Rohit", "virat"];// array
+
+let myObj = {
+    name: "sachin",
+    age: 22
+}
+
+const myfunction = function (){
+    console.log("hello world");
+}
 
 
