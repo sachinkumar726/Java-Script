931881b8e5bd2f7afaffef6320d30d55d132cd2e
Sachin Kumar <sachinmandal726@gmail.com>
2024-07-21 16:09:08 +0530

    projects

--- Projects/unlimited_colors/script.js
+++ Projects/unlimited_colors/script.js
@@ -1,28 +1,37 @@
-//generate a random color
-const randomColor = function () {
-    const hex = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-      color += hex[Math.floor(Math.random() * 16)];
-    }
-    return color;
-  };
-  
-  let IntervalID;
-  const startChangingColor = function () {
-    if (!IntervalID) {
-      IntervalID = setInterval(changeBgcolor, 1000);
-    }
-    function changeBgcolor() {
-      document.body.style.backgroundColor = randomColor();
-    }
-  };
-  
-  const stopChanginColor = function () {
-    clearInterval(IntervalID);
-    IntervalID = null;
-  };
-  document.querySelector('#start').addEventListener('click', startChangingColor);
-  
-  document.querySelector('#stop').addEventListener('click', stopChanginColor);
-f  
+//
+
+const randomcolor = function(){
+
+  const hex = '0123456789ABCDRF';
+  let color = '#';
+
+  for(let i=0; i<6; i++){
+    color += hex[Math.floor(Math.random() *16)];
+
+  }
+  return color;
+}
+
+let IntervalId;
+
+const startChangingColor = function(){
+  if(!IntervalId){
+    IntervalId = setInterval(changeBgcolor, 1000);
+  }
+
+  function changeBgcolor(){
+    document.body.style.backgroundColor= randomcolor();
+  }
+
+};
+
+const stopChangingColor = function(){
+  clearInterval(IntervalId);
+  IntervalId = null;
+}
+
+let start = document.querySelector('#start');
+let stop = document.querySelector('#stop');
+
+start.addEventListener('click', startChangingColor);
+stop.addEventListener('click', stopChangingColor);
